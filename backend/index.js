e8c9276fcaf0e449363ed0a0cf118304e8c764af
KoolKrish18 <nalamkrish1@gmail.com>
2025-03-05 20:07:51 -0500

    Create database & Connect to Sequlize

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,11 @@
-const express = require('express')
+import express from 'express'
+import sqlite3 from 'sqlite3'
+import { Sequelize } from 'sequelize'
+
+new sqlite3.Database('./chatApp.db')
+
+const sequelize = new Sequelize({ dialect: 'sqlite', storage: './chatApp.db' })
+
 const app = express()
 const port = 3000
 
@@ -9,3 +16,11 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+//TEST: connection between sequlize and database
+try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+} catch (error) {
+    console.error('Unable to connect to the database:', error)
+}
