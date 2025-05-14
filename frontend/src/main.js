19108175e17db061acecbee1ad084b18c2786ce0
KoolKrish18 <nalamkrish1@gmail.com>
2025-05-14 12:45:57 -0400

    Finished messaging UI

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', runAll)
+document.addEventListener('DOMContentLoaded', load)
 
-function loadUsers() {
+function load() {
     fetch('http://localhost:3000/user/readAll', { method: 'GET' })
         .then((response) => {
             if (!response.ok) {
@@ -9,8 +9,6 @@ function loadUsers() {
             return response.json()
         })
         .then((json) => {
-            document.getElementById('users').innerHTML = ''
-            document.getElementById('messages').innerHTML = ''
             for (const x of json.allUsers) {
                 const curr = document.createElement('div')
                 const currContent = document.createElement('div')
@@ -18,26 +16,86 @@ function loadUsers() {
                 curr.classList.add('user')
                 currContent.id = x.name
                 currContent.innerText = x.name
-                currContent.classList.add('msg')
+                currContent.classList.add('header')
                 curr.addEventListener('click', () => {
                     let i, allHistory, currEffect
-                    allHistory = document.getElementsByClassName('msg')
+                    allHistory = document.getElementsByClassName('header')
+                    currEffect = document.getElementsByClassName('user')
                     for (i = 0; i < allHistory.length; i++) {
                         allHistory[i].style.display = 'none'
-                    }
-                    currEffect = document.getElementsByClassName('user')
-                    for (i = 0; i < currEffect.length; i++) {
                         currEffect[i].className = currEffect[
                             i
                         ].className.replace(' active', '')
                     }
-                    document.getElementById(x.name).style.display = 'unset'
+                    document.getElementById(x.name).style.display = 'flex'
                     curr.className += ' active'
+                    loadMessages(x.name)
                 })
-                document.getElementById('users').appendChild(curr)
-                document.getElementById('messages').appendChild(currContent)
+
+                document.getElementById('sidebar').appendChild(curr)
+                document.getElementById('display').appendChild(currContent)
             }
         })
+}
+
+function loadMessages(name) {
+    const tester = {
+        Dave: [
+            {
+                You: 'dew',
+            },
+            {
+                Sender: 'Lmao',
+            },
+        ],
+        Krish: [
+            {
+                You: 'Bruh',
+            },
+            {
+                Sender: 'Lmao',
+            },
+        ],
+        Mridul: [
+            {
+                You: 'Bruh',
+            },
+            {
+                Sender: 'Lmao',
+            },
+        ],
+        Mohammad: [
+            {
+                You: 'Bruh',
+            },
+            {
+                Sender: 'Lmao',
+            },
+        ],
+    }
+    document.getElementById('messages').innerHTML = ''
+    if (tester[name]) {
+        for (let i = 0; i < tester[name].length; i++) {
+            let msg = document.createElement('div')
+            if (tester[name][i].You) {
+                msg.innerText = tester[name][i].You
+                msg.classList.add('you')
+            }
+            if (tester[name][i].Sender) {
+                msg.innerText = tester[name][i].Sender
+                msg.classList.add('sender')
+            }
+            document.getElementById('messages').appendChild(msg)
+        }
+    } else {
+        const first = document.createElement('div')
+        first.innerText = 'Send a message to start a conversation'
+        first.classList.add('default')
+        document.getElementById('messages').appendChild(first)
+    }
+    const send = document.createElement('input')
+    send.placeholder = 'Send a message...'
+    document.getElementById('messages').appendChild(send)
 }
 
 // add User
@@ -53,5 +111,5 @@ function loadUsers() {
 // )
 
 function runAll() {
-    loadUsers()
+    load()
 }
