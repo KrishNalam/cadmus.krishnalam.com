5824ccab29ad298522b24ec3cb594a44451f614b
KoolKrish18 <nalamkrish1@gmail.com>
2025-06-10 21:46:49 -0400

    Use dotenvx (Not needed due to render)

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import sqlite3 from 'sqlite3'
 import { Sequelize } from 'sequelize'
 import userRoute from './api/routes/userRoute.js'
 import chatRoute from './api/routes/chatRoute.js'
+import('@dotenvx/dotenvx/config')
 
 new sqlite3.Database('./chatApp.db')
 
@@ -24,7 +25,7 @@ try {
 const app = express()
 const port = 8080
 
-var allowlist = ['http://localhost:5173', 'https://cadmus.krishnalam.com']
+var allowlist = [process.env.LOCAL, process.env.PROD]
 
 const corsOptionsDelegate = (req, callback) => {
     const origin = req.header('Origin')
